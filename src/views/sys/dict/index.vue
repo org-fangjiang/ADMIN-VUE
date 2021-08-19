b7bc3467994fc117a11932f088a53e278a9514fc
lilongsha <lilongsha@163.com>
2021-08-19 14:36:24 +0800

    feat: 添加地区管理页面、地铁管理页面、字典管理页面、商圈管理页面

---
+++ src/views/sys/dict/index.vue
@@ -0,0 +1,11 @@
+// 字典信息管理页面
+
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style></style>
---
+++ src/views/sys/metro/index.vue
@@ -0,0 +1,11 @@
+// 地铁信息管理页面
+
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style></style>
