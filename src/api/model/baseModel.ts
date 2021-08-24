95f519a3d121bc2373a70608d67ad4e737c63380
lilongsha <lilongsha@163.com>
2021-08-24 11:11:01 +0800

    feat: 添加响应结果基本类型

--- src/api/model/baseModel.ts
+++ src/api/model/baseModel.ts
@@ -7,3 +7,36 @@ export interface BasicFetchResult<T extends any> {
   items: T[];
   total: number;
 }
+
+export enum PageSizeEnum {
+  A = 10,
+  B = 20,
+  C = 30,
+  D = 50,
+  E = 80,
+  F = 100,
+}
+
+export const PageSizeList: string[] = ['2', '10', '20', '30', '50', '80', '100'];
+export interface BasePageResult<T extends any> {
+  page: {
+    size: number;
+    number: number;
+    numberOfElements: number;
+    totalPages: number;
+    totalElements: number;
+  };
+  content: T[];
+}
+
+export interface BaseResult<T extends any> {
+  content: T;
+}
+export interface PageParam {
+  pageNum: number;
+  pageSize: number;
+}
+export interface SortParam {
+  asc: string[];
+  desc: string[];
+}
