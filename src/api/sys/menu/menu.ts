c51418857a7ec48095324970d756cd89921f7b03
lilongsha <lilongsha@163.com>
2021-09-01 11:59:30 +0800

    feat: 添加菜单权限接口信息

---
+++ src/api/sys/menu/menu.ts
@@ -0,0 +1,136 @@
+import { defHttp } from '/@/utils/http/axios';
+import { ErrorMessageMode } from '/#/axios';
+import { BasePageResult, BaseResult } from '../../model/baseModel';
+import { RequestParam } from '/@/utils/param/requestParam';
+import { PageParam, SortParam } from '/@/api/model/baseModel';
+import { MenuByCondition, MenuModel } from './model/menuModel';
+import { MenuMode } from 'ant-design-vue/lib/menu/src/interface';
+
+enum Api {
+  GetParentMenu = '/sys-server/menu/getParentMenu',
+  GetChildMenu = '/sys-server/menu/getChildMenu',
+  GetMenu = '/sys-server/menu/getMenu',
+  AddMenu = '/sys-server/menu/addMenu',
+  UpdateMenu = '/sys-server/menu/updateMenu',
+  DeleteMenu = '/sys-server/menu/deleteMenu',
+  ReEnableMenu = '/sys-server/menu/reEnableMenu',
+}
+
+export function getParentMenu(
+  condition: MenuByCondition,
+  page?: PageParam,
+  sort?: SortParam,
+  mode: ErrorMessageMode = 'modal'
+) {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  reqParam.setPage(page);
+  reqParam.setSort(sort);
+  const data = reqParam.getInstance();
+  return defHttp.post<BasePageResult<MenuModel>>(
+    {
+      url: Api.GetParentMenu,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+export function getChildMenu(
+  condition: MenuByCondition,
+  page?: PageParam,
+  sort?: SortParam,
+  mode: ErrorMessageMode = 'modal'
+) {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  reqParam.setPage(page);
+  reqParam.setSort(sort);
+  const data = reqParam.getInstance();
+  return defHttp.post<BasePageResult<MenuModel>>(
+    {
+      url: Api.GetChildMenu,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+export function getMenu(condition: MenuByCondition, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<MenuModel>>(
+    {
+      url: Api.GetMenu,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+export function addMenu(menu: MenuMode, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(menu);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<MenuModel>>(
+    {
+      url: Api.AddMenu,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+export function updateMenu(menu: MenuMode, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(menu);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<MenuModel>>(
+    {
+      url: Api.UpdateMenu,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+export function deleteMenu(menu: MenuMode, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(menu);
+  const data = reqParam.getInstance();
+  return defHttp.delete<BaseResult<MenuModel>>(
+    {
+      url: Api.DeleteMenu,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+export function reEnableMenu(menu: MenuMode, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(menu);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<MenuModel>>(
+    {
+      url: Api.ReEnableMenu,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
--- src/api/sys/menu/model/menuModel.ts
+++ src/api/sys/menu/model/menuModel.ts
@@ -1,3 +1,5 @@
+import { useI18n } from '/@/hooks/web/useI18n';
+const { t } = useI18n();
 export interface MenuModel {
   id: string;
   parentId: string;
@@ -14,8 +16,150 @@ export interface MenuModel {
   state: string;
 }
 
+export interface MenuByCondition {
+  id?: string; // 菜单id
+  parentId?: string; // 父级菜单id
+  state?: string; // 菜单状态 查询菜单详情时此项不需要
+}
+
 export const MenuConst = {
   EFFECTIVE: '1',
   INVALID: '0',
-  STATES: ['0', '1'],
+  STATES: [
+    { value: '0', label: t(''), color: 'volcano' },
+    { value: '1', label: t(''), color: 'geekblue' },
+  ],
+  _TYPE: [
+    { value: '0', label: t(''), color: 'volcano' }, // 菜单
+    { value: '1', label: t(''), color: 'geekblue' }, // 按钮
+  ],
+  _ADD_MENU_FIELDS: ['parentId', 'menuName', 'path', 'component', 'icon', 'type', 'orderNum'],
+  _ADD_BUTTON_FIELDS: [
+    'parentId',
+    'menuName',
+    'path',
+    'component',
+    'perms',
+    'icon',
+    'type',
+    'orderNum',
+  ],
+  _UPDATE_MENU_FIELDS: [
+    'parentId',
+    'menuName',
+    'path',
+    'component',
+    'icon',
+    'type',
+    'orderNum',
+    'state',
+  ],
+  _UPDATE_BUTTON_FIELDS: [
+    'parentId',
+    'menuName',
+    'path',
+    'component',
+    'perms',
+    'icon',
+    'type',
+    'orderNum',
+    'state',
+  ],
+  _MENU_RULES: {
+    parentId: [{ required: true, message: t(''), trigger: 'blur' }],
+    menuName: [{ required: true, message: t(''), trigger: 'blur' }],
+    path: [{ required: true, message: t(''), trigger: 'blur' }],
+    component: [{ required: false, message: t(''), trigger: 'blur' }],
+    perms: [{ required: false, message: t(''), trigger: 'blur' }],
+    icon: [{ required: false, message: t(''), trigger: 'blur' }],
+    type: [{ required: true, message: t(''), trigger: 'blur' }],
+    orderNum: [{ required: false, message: t(''), trigger: 'blur' }],
+    createTime: [{ required: false, message: t(''), trigger: 'blur' }],
+    updateTime: [{ required: false, message: t(''), trigger: 'blur' }],
+    createBy: [{ required: false, message: t(''), trigger: 'blur' }],
+    state: [{ required: true, message: t(''), trigger: 'blur' }],
+  },
+  _BUTTON_RULES: {
+    parentId: [{ required: true, message: t(''), trigger: 'blur' }],
+    menuName: [{ required: true, message: t(''), trigger: 'blur' }],
+    path: [{ required: true, message: t(''), trigger: 'blur' }],
+    component: [{ required: false, message: t(''), trigger: 'blur' }],
+    perms: [{ required: false, message: t(''), trigger: 'blur' }],
+    icon: [{ required: false, message: t(''), trigger: 'blur' }],
+    type: [{ required: true, message: t(''), trigger: 'blur' }],
+    orderNum: [{ required: false, message: t(''), trigger: 'blur' }],
+    createTime: [{ required: false, message: t(''), trigger: 'blur' }],
+    updateTime: [{ required: false, message: t(''), trigger: 'blur' }],
+    createBy: [{ required: false, message: t(''), trigger: 'blur' }],
+    state: [{ required: true, message: t(''), trigger: 'blur' }],
+  },
+  _PERMS: {
+    SELECT: 'menu:select',
+    ADD: 'menu:add',
+    UPDATE: 'menu:update',
+    DELETE: 'menu:delete',
+  },
 };
+
+export const Columns = [
+  {
+    title: t(''),
+    key: 'menuName',
+    dataIndex: 'menuName',
+    ellipsis: 'true',
+    slots: '',
+  },
+  {
+    title: t(''),
+    key: 'path',
+    dataIndex: 'path',
+    ellipsis: 'true',
+    slots: '',
+  },
+  {
+    title: t(''),
+    key: 'perms',
+    dataIndex: 'perms',
+    ellipsis: 'true',
+    slots: '',
+  },
+  {
+    title: t(''),
+    key: 'icon',
+    dataIndex: 'icon',
+    ellipsis: 'true',
+    slots: '',
+  },
+  {
+    title: t(''),
+    key: 'type',
+    dataIndex: 'type',
+    ellipsis: 'true',
+    slots: '',
+  },
+  {
+    title: t(''),
+    key: 'state',
+    dataIndex: 'state',
+    ellipsis: 'true',
+    slots: '',
+  },
+  {
+    title: t(''),
+    key: 'action',
+    ellipsis: 'true',
+    slots: { customRender: 'action' },
+  },
+];
+
+// SYS_MENU_MISS_ID(4049, "菜单权限参数错误：编号"),
+// SYS_MENU_MISS_MENU_NAME(4049, "菜单权限参数错误：名称"),
+// SYS_MENU_MISS_PATH(4049, "菜单权限参数错误：路径"),
+// SYS_MENU_MISS_TYPE(4049, "菜单权限参数错误：类型"),
+// SYS_MENU_MISS_PERMS(4049, "菜单权限参数错误：权限标识符"),
+// SYS_MENU_MISS_PARENT_ID(4049, "菜单权限参数错误：上级编号"),
+// SYS_MENU_MISS_STATE(4049, "菜单权限参数错误：状态"),
+// SYS_MENU_IS_EXIST(4009, "菜单权限已存在"),
+// SYS_MENU_IS_NOT_EXIST(4010, "菜单权限不存在"),
+// SYS_MENU_PARENT_IS_EXIST(4019, "菜单权限上级已存在"),
+// SYS_MENU_PARENT_IS_NOT_EXIST(4020, "菜单权限上级不存在"),
