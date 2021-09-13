1f8afc673e99be5a07f76cfc652af6244ef7f2f5
lilongsha <lilongsha@163.com>
2021-09-13 11:19:45 +0800

    feat: 用户接口信息

---
+++ src/api/sys/user/model/userModel.ts
@@ -0,0 +1,150 @@
+import { useI18n } from '/@/hooks/web/useI18n';
+const { t } = useI18n();
+
+export interface AddUserModel {
+  username?: string;
+  password?: string;
+  realName?: string;
+  nickName?: string;
+  sex?: string;
+  email?: string;
+  mobile?: string;
+  theme?: string;
+  avatar?: string;
+  description?: string;
+  deptId?: string;
+  deptName?: string;
+  roleId?: string;
+  roleName?: string;
+  sysRoleBeans?: string[]; // roleId 集合
+  type?: string;
+  companyName?: string;
+  companyId?: string;
+}
+
+export interface GetUserModel {
+  id?: string;
+  username?: string;
+  password?: string;
+  realName?: string;
+  nickName?: string;
+  companyId?: string;
+  deptId?: string;
+  email?: string;
+  mobile?: string;
+  state?: string;
+  lastLoginTime?: string;
+  sex?: string;
+  theme?: string;
+  avatar?: string;
+  description?: string;
+  createTime?: string;
+  createBy?: string;
+  updateTime?: string;
+  sysUserRolesById?: string;
+}
+
+// getAllUsers 接口使用 SUPER_ADMIN
+export interface GetAllUsersCondition {
+  companyId: string;
+  state?: string;
+}
+
+// getAllUsersByRole 接口使用 SUPER_ADMIN
+export interface GetAllUsersByRoleCondition {
+  companyId: string;
+  roleId: string;
+  state?: string;
+}
+
+// getUsers 接口使用 user:select
+export interface GetUsersCondition {
+  deptId?: string;
+  state?: string;
+}
+
+// getUsersByMobile 接口使用 user:select
+export interface GetUsersByMobileCondition {
+  mobile: string;
+  state?: string;
+}
+
+// getUsersByRole 接口使用 user:select
+export interface GetUsersByRoleCondition {
+  roleId: string;
+  state?: string;
+}
+
+// updateMyInfo 接口使用
+export interface UpdateMyInfo {
+  id: string;
+  realName?: string;
+  nickName?: string;
+  sex?: string;
+  avatar?: string;
+  description?: string;
+}
+
+// updateUserInfo 接口使用
+export interface UpdateUserInfo {
+  id: string;
+  realName?: string;
+  nickName?: string;
+  sex?: string;
+  avatar?: string;
+  description?: string;
+  password?: string;
+}
+
+export const _Const = {
+  EFFECTIVE: '1',
+  INVALID: '0',
+  STATES: [
+    { value: '0', label: t('model.role.invalid'), color: 'volcano' },
+    { value: '1', label: t('model.role.effective'), color: 'geekblue' },
+  ],
+  MAN: '0',
+  WOMAN: '1',
+  NO: '2',
+  _ADD_FIELDS: [
+    'username',
+    'password',
+    'realName',
+    'nickName',
+    'sex',
+    'email',
+    'mobile',
+    'state',
+    'theme',
+    'avatar',
+    'description',
+    'deptId',
+    'deptName',
+    'roleId',
+    'roleName',
+    'sysRoleBeans',
+  ],
+  _UPDATE_FIELDS: ['realName', 'nickName', 'sex', 'avatar', 'description', 'password'],
+  _RULES: {
+    password: [{ required: true, message: t(''), trigger: 'blur' }],
+    realName: [{ required: true, message: t(''), trigger: 'blur' }],
+    nickName: [{ required: false, message: t(''), trigger: 'blur' }],
+    sex: [{ required: false, message: t(''), trigger: 'blur' }],
+    email: [{ required: false, message: t(''), trigger: 'blur' }],
+    mobile: [{ required: true, message: t(''), trigger: 'blur' }],
+    theme: [{ required: false, message: t(''), trigger: 'blur' }],
+    avatar: [{ required: false, message: t(''), trigger: 'blur' }],
+    description: [{ required: false, message: t(''), trigger: 'blur' }],
+    deptId: [{ required: false, message: t(''), trigger: 'blur' }],
+    deptName: [{ required: false, message: t(''), trigger: 'blur' }],
+    roleId: [{ required: false, message: t(''), trigger: 'blur' }],
+    roleName: [{ required: false, message: t(''), trigger: 'blur' }],
+    sysRoleBeans: [{ required: false, message: t(''), trigger: 'blur' }],
+  },
+  _PERMS: {
+    SELECT: 'user:select',
+    ADD: 'user:add',
+    UPDATE: 'user:update',
+    DELETE: 'user:delete',
+  },
+};
---
+++ src/api/sys/user/user.ts
@@ -0,0 +1,336 @@
+import { defHttp } from '/@/utils/http/axios';
+import { ErrorMessageMode } from '/#/axios';
+import { BasePageResult, BaseResult } from '../../model/baseModel';
+import { RequestParam } from '/@/utils/param/requestParam';
+import { PageParam, SortParam } from '/@/api/model/baseModel';
+import {
+  AddUserModel,
+  GetAllUsersByRoleCondition,
+  GetAllUsersCondition,
+  GetUserModel,
+  GetUsersByMobileCondition,
+  GetUsersByRoleCondition,
+  GetUsersCondition,
+  UpdateMyInfo,
+  UpdateUserInfo,
+} from './model/userModel';
+
+enum Api {
+  GetAllUsers = '/sys-server/user/getAllUsers', // SUPER_ADMIN
+  GetAllUsersByRole = '/sys-server/user/getAllUsersByRole', // SUPER_ADMIN
+  AddCompanyUser = '/sys-server/user/addCompanyUser', // SUPER_ADMIN
+  DeleteCompanyUser = '/sys-server/user/deleteCompanyUser', // SUPER_ADMIN
+  GetUsers = '/sys-server/user/getUsers', // user:select
+  GetUsersByMobile = '/sys-server/user/getUsersByMobile', // user:select
+  GetUsersByRole = '/sys-server/user/getUsersByRole', // user:select
+  AddUser = '/sys-server/user/addUser', // user:add
+  UpdateMyInfo = '/sys-server/user/updateMyInfo', // user:update
+  UpdateUserInfo = '/sys-server/user/updateUserInfo', // user:update
+  DeleteUser = '/sys-server/user/deleteUser', // user:delete
+  SetUserRole = '/sys-server/user/setUserRole', // user:update
+  SetUserDeptId = '/sys-server/user/setUserDeptId', // user:update
+  SetUserMobile = '/sys-server/user/setUserMobile', // user:update
+  SetUserEmail = '/sys-server/user/setUserEmail', // user:update
+  ReEnableUser = '/sys-server/user/reEnableUser', // user:update
+}
+// 恢复用户信息
+export function reEnableUser(id: string, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData({
+    id,
+  });
+  const data = reqParam.getInstance();
+  return defHttp.delete<BaseResult<AddUserModel>>(
+    {
+      url: Api.ReEnableUser,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+// 设置用户部门
+export function setUserEmail(id: string, email: string, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData({
+    id,
+    email,
+  });
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.SetUserEmail,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+// 设置用户手机号
+export function setUserMobile(id: string, mobile: string, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData({
+    id,
+    mobile,
+  });
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.SetUserMobile,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+// 设置用户部门
+export function setUserDeptId(id: string, deptId: string, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData({
+    id,
+    deptId,
+  });
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.SetUserDeptId,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+// 设置用户角色
+export function setUserRole(id: string, sysRoleBeans: string[], mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData({
+    id,
+    sysRoleBeans,
+  });
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.SetUserRole,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+// 删除用户信息
+export function deleteUser(id: string, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData({
+    id,
+  });
+  const data = reqParam.getInstance();
+  return defHttp.delete<BaseResult<AddUserModel>>(
+    {
+      url: Api.DeleteUser,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+// 更新用户信息
+export function updateUserInfo(user: UpdateUserInfo, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(user);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.UpdateUserInfo,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+// 更新个人信息
+export function updateMyInfo(user: UpdateMyInfo, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(user);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.UpdateMyInfo,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 添加用户
+export function addUser(user: AddUserModel, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(user);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.AddUser,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 获取自己企业下所有的相似手机号用户
+export function getUsersByRole(
+  condition: GetUsersByRoleCondition,
+  page?: PageParam,
+  sort?: SortParam,
+  mode: ErrorMessageMode = 'modal'
+) {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  reqParam.setPage(page);
+  reqParam.setSort(sort);
+  const data = reqParam.getInstance();
+  return defHttp.post<BasePageResult<GetUserModel>>(
+    {
+      url: Api.GetUsersByRole,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 获取自己企业下所有的相似手机号用户
+export function getUsersByMobile(
+  condition: GetUsersByMobileCondition,
+  page?: PageParam,
+  sort?: SortParam,
+  mode: ErrorMessageMode = 'modal'
+) {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  reqParam.setPage(page);
+  reqParam.setSort(sort);
+  const data = reqParam.getInstance();
+  return defHttp.post<BasePageResult<GetUserModel>>(
+    {
+      url: Api.GetUsersByMobile,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 获取自己企业下的所有用户
+export function getUsers(
+  condition: GetUsersCondition,
+  page?: PageParam,
+  sort?: SortParam,
+  mode: ErrorMessageMode = 'modal'
+) {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  reqParam.setPage(page);
+  reqParam.setSort(sort);
+  const data = reqParam.getInstance();
+  return defHttp.post<BasePageResult<GetUserModel>>(
+    {
+      url: Api.GetUsers,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 获取指定企业下的所有用户
+export function getAllUsers(
+  condition: GetAllUsersCondition,
+  page?: PageParam,
+  sort?: SortParam,
+  mode: ErrorMessageMode = 'modal'
+) {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  reqParam.setPage(page);
+  reqParam.setSort(sort);
+  const data = reqParam.getInstance();
+  return defHttp.post<BasePageResult<GetUserModel>>(
+    {
+      url: Api.GetAllUsers,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 获取指定企业下指定的角色用户
+export function getAllUsersByRole(
+  condition: GetAllUsersByRoleCondition,
+  page?: PageParam,
+  sort?: SortParam,
+  mode: ErrorMessageMode = 'modal'
+) {
+  const reqParam = new RequestParam();
+  reqParam.setData(condition);
+  reqParam.setPage(page);
+  reqParam.setSort(sort);
+  const data = reqParam.getInstance();
+  return defHttp.post<BasePageResult<GetUserModel>>(
+    {
+      url: Api.GetAllUsersByRole,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 超级管理员添加企业管理员
+export function addCompanyUser(user: AddUserModel, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData(user);
+  const data = reqParam.getInstance();
+  return defHttp.post<BaseResult<AddUserModel>>(
+    {
+      url: Api.AddCompanyUser,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+// 超级管理员删除企业管理员
+export function deleteCompanyUser(id: string, mode: ErrorMessageMode = 'modal') {
+  const reqParam = new RequestParam();
+  reqParam.setData({
+    id,
+  });
+  const data = reqParam.getInstance();
+  return defHttp.delete<BaseResult<AddUserModel>>(
+    {
+      url: Api.DeleteCompanyUser,
+      data,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
