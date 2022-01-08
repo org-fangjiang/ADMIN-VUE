e38fea38f0eb075b88e5932e0259f11307d3b648
13582116306wyy163com <wyy13582116306@163.com>
2022-01-08 14:14:49 +0800

    feat: 添加时间规则菜单，超级管理员进行添加

---
+++ src/views/marketing/allTimeRule/components/RuleForm.vue
@@ -0,0 +1,218 @@
+<template>
+  <div :class="prefixCls">
+    <Form
+      ref="formRef"
+      :model="formState"
+      :rules="ruleConst._RULES"
+      :label-col="labelCol"
+      :wrapper-col="wrapperCol"
+    >
+      <FormItem ref="companyId" :label="t('marketing.timeRule.companyId')" name="companyId">
+        <Select
+          v-model:value="formState.companyId"
+          ref="selectRef"
+          label-in-value
+          style="width: 120px"
+          :filter-option="false"
+          :not-found-content="null"
+          :options="options"
+          @search="fetchUser"
+          :showSearch="true"
+          :labelInValue="false"
+          @change="companyChange"
+          :allowClear="true"
+          :disabled="isUpdate"
+        />
+      </FormItem>
+      <FormItem
+        ref="clueDropLevel"
+        :label="t('marketing.timeRule.clueDropLevel')"
+        name="clueDropLevel"
+      >
+        <InputNumber
+          :disabled="isUpdate && !ruleConst._UPDATE_FIELDS.includes('clueDropLevel')"
+          v-model:value="formState.clueDropLevel"
+          autoComplete="off"
+        />
+      </FormItem>
+      <FormItem
+        ref="clueUnFollow"
+        :label="t('marketing.timeRule.clueUnFollow')"
+        name="clueUnFollow"
+      >
+        <InputNumber
+          :disabled="isUpdate && !ruleConst._UPDATE_FIELDS.includes('clueUnFollow')"
+          v-model:value="formState.clueUnFollow"
+          autoComplete="off"
+        />
+      </FormItem>
+      <FormItem
+        ref="clueUnclaimed"
+        :label="t('marketing.timeRule.clueUnclaimed')"
+        name="clueUnclaimed"
+      >
+        <InputNumber
+          :disabled="isUpdate && !ruleConst._UPDATE_FIELDS.includes('clueUnclaimed')"
+          v-model:value="formState.clueUnclaimed"
+          autoComplete="off"
+        />
+      </FormItem>
+      <FormItem :wrapper-col="{ span: 14, offset: 8 }">
+        <Button type="primary" @click="onSubmit">{{ t('component.modal.okText') }}</Button>
+        <Button style="margin-left: 10px" @click="resetForm">{{
+          t('component.cropper.btn_reset')
+        }}</Button>
+      </FormItem>
+    </Form>
+    <Loading :loading="loading" :absolute="false" :tip="tip" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { useI18n } from '/@/hooks/web/useI18n';
+  import { useDesign } from '/@/hooks/web/useDesign';
+  import { defineComponent, onMounted, reactive, ref, UnwrapRef } from 'vue';
+  import { Button, Form, FormItem, InputNumber, Select } from 'ant-design-vue';
+  import { Loading } from '/@/components/Loading';
+  import { success, failed } from '/@/hooks/web/useList';
+  import { TimeRuleConst, TimeRuleModel } from '/@/api/marketing/timeRule/model/timeRuleModel';
+  import { add, get, update } from '/@/api/marketing/timeRule/timeRule';
+  import { debounce } from 'lodash-es';
+  import { getCompanies } from '/@/api/sys/compnay/company';
+
+  interface Option {
+    value: string;
+    label: string;
+  }
+  export default defineComponent({
+    name: 'RuleForm',
+    components: {
+      Button,
+      Form,
+      FormItem,
+      InputNumber,
+      Loading,
+      Select,
+    },
+    props: {
+      id: {
+        type: String,
+        require: true,
+      },
+    },
+    setup(props) {
+      const { t } = useI18n();
+      const { prefixCls } = useDesign('timeRule');
+      const ruleConst = ref(TimeRuleConst);
+      let loading = ref<boolean>(true);
+      let tip = ref<string>('加载中...');
+
+      //判断更新还是新增
+      let isUpdate = ref<boolean>(false);
+      if (props.id && props.id !== '') {
+        isUpdate.value = true;
+      }
+
+      // fromRef 获取form
+      const formRef = ref();
+      const formState: UnwrapRef<TimeRuleModel> = reactive({});
+
+      const options = ref<Option[]>([]);
+      //筛选文本框值发生变化调用
+      const fetchUser = debounce(async (value) => {
+        options.value.splice(0);
+        const { content } = await getCompanies({ name: value });
+        if (content) {
+          content.forEach((item) => {
+            options.value?.push({ value: item.id || '', label: item.name || '' });
+          });
+        }
+      }, 200);
+
+      //选择企业
+      const companyChange = async (value) => {
+        formState.companyId = value;
+      };
+
+      const onSubmit = () => {
+        formRef.value
+          .validate()
+          .then(async () => {
+            if (props.id) {
+              loading.value = true;
+              try {
+                formState.companyId = props.id;
+                await update(formState);
+                success(t('marketing.action.update'), t('marketing.action.success'));
+              } catch (error: any) {
+                failed(error?.response?.data?.message, t('marketing.action.fail'));
+              } finally {
+                loading.value = false;
+              }
+            } else {
+              loading.value = true;
+              try {
+                const { content } = await add(formState);
+                success(t('marketing.action.add'), t('marketing.action.success'));
+                Object.assign(formState, content);
+              } catch (error: any) {
+              } finally {
+                loading.value = false;
+              }
+            }
+          })
+          .catch((error: any) => {
+            console.log('error', error);
+          });
+      };
+
+      //重置
+      const resetForm = async () => {
+        loading.value = true;
+        if (props.id) {
+          try {
+            const { content } = await get({ companyId: props.id });
+            Object.assign(formState, content);
+          } catch (error) {
+          } finally {
+            loading.value = false;
+          }
+        } else {
+          formRef.value.resetFields();
+        }
+      };
+
+      //初始加载
+      onMounted(async () => {
+        loading.value = true;
+        if (props.id) {
+          const { content } = await get({ companyId: props.id });
+          Object.assign(formState, content);
+          if (content.companyById) {
+            options.value.push({ value: content.companyById.id, label: content.companyById.name });
+          }
+        }
+        loading.value = false;
+      });
+
+      return {
+        t,
+        prefixCls,
+        ruleConst,
+        loading,
+        tip,
+        formRef,
+        formState,
+        onSubmit,
+        resetForm,
+        labelCol: { span: 13 },
+        wrapperCol: { span: 14 },
+        isUpdate,
+        props,
+        companyChange,
+        fetchUser,
+        options,
+      };
+    },
+  });
+</script>
---
+++ src/views/marketing/allTimeRule/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="p-4">
+    <Button class="my-4" type="default" @click="addRule">{{ t('host.action.add') }}</Button>
+    <Table :columns="columns" :data-source="list" rowKey="companyId" :pagination="false">
+      <template #companyId="{ text: comId }">
+        <span>{{ comId.companyById?.name || comId.companyId }}</span>
+      </template>
+      <template #operation="{ text: link }">
+        <Button type="link" @click="updateRule(link)">{{ t('host.action.update') }}</Button>
+      </template>
+    </Table>
+    <Pagination
+      show-size-changer
+      size="large"
+      :show-total="(total) => t('component.table.total', { total })"
+      :pageSizeOptions="pageSizeList"
+      :current="pageParam.number"
+      :pageSize="pageParam.size"
+      :total="pageParam.totalElements"
+      @change="onChange"
+      @showSizeChange="onShowSizeChange"
+    />
+    <Modal
+      v-model:visible="drawerParam.visible"
+      :title="drawerParam.title"
+      @cancel="onClose"
+      :destroyOnClose="true"
+      :footer="null"
+      :bodyStyle="{ overflowY: 'auto', margin: '16px' }"
+    >
+      <RuleForm v-if="drawerParam.state === '0'" :id="drawerParam.id" />
+    </Modal>
+    <Loading :loading="loading" :absolute="false" :tip="tip" />
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent, onMounted, reactive, ref } from 'vue';
+  import {
+    TimeRuleModel,
+    TimeRuleConst as timeRuleConst,
+  } from '/@/api/marketing/timeRule/model/timeRuleModel';
+  import { processList } from '/@/hooks/web/useList';
+  import { useI18n } from 'vue-i18n';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import { useUserStore } from '/@/store/modules/user';
+  import { Loading } from '/@/components/Loading';
+  import { BasePageResult, PageSizeList } from '/@/api/model/baseModel';
+  import { getByPage } from '/@/api/marketing/timeRule/timeRule';
+  import { TimeRuleColumns } from '/@/api/marketing/timeRule/model/timeRuleModel';
+  import { useDesign } from '/@/hooks/web/useDesign';
+  import { Button, Table, Pagination, Modal } from 'ant-design-vue';
+  import RuleForm from './components/RuleForm.vue';
+
+  export default defineComponent({
+    name: 'TimeRuleForm',
+    components: {
+      Loading,
+      Button,
+      Pagination,
+      Table,
+      Modal,
+      RuleForm,
+    },
+    setup() {
+      const { t } = useI18n();
+      const { createErrorModal } = useMessage();
+      let loading = ref<boolean>(true);
+      let tip = ref<string>('加载中...');
+      const pageSizeList = ref<string[]>(PageSizeList);
+      const { prefixCls } = useDesign('timeRule');
+      // 获取用户store
+      const userStore = useUserStore();
+      const companyId = userStore.getUserInfo.companyId;
+      const columns = reactive(TimeRuleColumns);
+      //分页参数
+      let pageParam = reactive({
+        size: 10,
+        number: 1,
+        numberOfElements: 0,
+        totalPages: 0,
+        totalElements: 0,
+      });
+
+      //初始加载
+      onMounted(async () => {
+        const result = await getList();
+        processList(result, list, pageParam);
+      });
+
+      const getList = async () => {
+        loading.value = true;
+        let result: BasePageResult<TimeRuleModel> | undefined;
+        try {
+          result = await getByPage({
+            pageNum: pageParam.number,
+            pageSize: pageParam.size,
+          });
+        } catch (error: any) {
+          createErrorModal({
+            title: t('sys.api.errorTip'),
+            content: error?.response?.data?.message || t('sys.api.networkExceptionMsg'),
+            getContainer: () => document.body.querySelector(`.${prefixCls}`) || document.body,
+          });
+        } finally {
+          loading.value = false;
+        }
+        return result;
+      };
+
+      // 抽屉参数
+      const drawerParam = reactive({
+        id: '',
+        state: '',
+        title: '',
+        visible: false,
+      });
+
+      const addRule = () => {
+        drawerParam.visible = true;
+        drawerParam.state = '0';
+        drawerParam.title = t('marketing.action.add');
+        drawerParam.id = '';
+      };
+
+      const updateRule = (link) => {
+        drawerParam.visible = true;
+        drawerParam.state = '0';
+        drawerParam.title = t('marketing.action.update');
+        drawerParam.id = link.companyId;
+      };
+
+      const onClose = () => {
+        drawerParam.visible = false;
+        drawerParam.title = '';
+        drawerParam.id = '';
+        drawerParam.state = '';
+      };
+
+      const timeRule: TimeRuleModel[] = [];
+      // 列表结果
+      let list = reactive(timeRule);
+
+      const onChange = async (page) => {
+        pageParam.number = page;
+        const result = await getList();
+        processList(result, list, pageParam);
+      };
+      const onShowSizeChange = async (current, size) => {
+        console.log(current);
+        pageParam.size = size;
+        pageParam.number = 1;
+        const result = await getList();
+        processList(result, list, pageParam);
+      };
+
+      return {
+        t,
+        prefixCls,
+        loading,
+        tip,
+        pageSizeList,
+        companyId,
+        list,
+        timeRuleConst,
+        pageParam,
+        columns,
+        onChange,
+        onShowSizeChange,
+        drawerParam,
+        addRule,
+        updateRule,
+        onClose,
+      };
+    },
+  });
+</script>
