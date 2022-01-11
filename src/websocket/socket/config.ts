0b24393b7a29b2b7ba1b646e4ce76d5fb05001be
13582116306wyy163com <wyy13582116306@163.com>
2022-01-11 11:23:01 +0800

    fix: 配置信息地址修改

--- src/websocket/socket/config.ts
+++ src/websocket/socket/config.ts
@@ -1,4 +1,4 @@
-export const wsUrl = 'ws://192.168.0.83:9000/sys-websocket/ws';
+export const wsUrl = 'wss://api.fangjiang.com/sys-websocket/ws';
 export const wsPath = '/sys-websocket/ws';
 export const wsTransports = ['websocket'];
 export const wsQuery = {};
